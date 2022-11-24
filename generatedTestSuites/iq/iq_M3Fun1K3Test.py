6b2e54179460e01233527dfa733fe307bf22d9d0
qiqihannah <784960656@qq.com>
2022-11-24 12:33:48 +0100

    iq data

--- generatedTestSuites/iq/iq_M3Fun1K3Test.py
+++ generatedTestSuites/iq/iq_M3Fun1K3Test.py
@@ -1,89 +1,81 @@
 import unittest
 import sys
-sys.path.append("/Users/xinyi/Documents/qucat_experiment/iq")
+sys.path.append("/Users/xinyi/Documents/qucat_sample")
 from executing import execute_quantum_program
 
-class iq_M3Scenario1K3Test(unittest.TestCase):
+class iq_backup_M3Scenario1K3Test(unittest.TestCase):
 
-	def test_iq_M3_483(self):
-		input = '0111100011'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_90(self):
+		input = '0001011010'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_156(self):
-		input = '0010011100'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_917(self):
+		input = '1110010101'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_768(self):
-		input = '1100000000'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_291(self):
+		input = '0100100011'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_628(self):
+	def test_iq_backup_M3_1000(self):
+		input = '1111101000'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
+
+	def test_iq_backup_M3_77(self):
+		input = '0001001101'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
+
+	def test_iq_backup_M3_446(self):
+		input = '0110111110'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
+
+	def test_iq_backup_M3_628(self):
 		input = '1001110100'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_591(self):
-		input = '1001001111'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_247(self):
+		input = '0011110111'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_317(self):
-		input = '0100111101'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_128(self):
+		input = '0010000000'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_201(self):
-		input = '0011001001'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_571(self):
+		input = '1000111011'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_58(self):
-		input = '0000111010'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_793(self):
+		input = '1100011001'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_1006(self):
-		input = '1111101110'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_685(self):
+		input = '1010101101'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_787(self):
-		input = '1100010011'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_838(self):
+		input = '1101000110'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_470(self):
-		input = '0111010110'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_979(self):
+		input = '1111010011'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_933(self):
-		input = '1110100101'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_476(self):
+		input = '0111011100'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_674(self):
-		input = '1010100010'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_526(self):
+		input = '1000001110'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_721(self):
-		input = '1011010001'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_650(self):
+		input = '1010001010'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
-	def test_iq_M3_553(self):
-		input = '1000101001'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
-
-	def test_iq_M3_888(self):
-		input = '1101111000'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
-
-	def test_iq_M3_4(self):
-		input = '0000000100'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
-
-	def test_iq_M3_287(self):
-		input = '0100011111'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
-
-	def test_iq_M3_340(self):
-		input = '0101010100'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
-
-	def test_iq_M3_183(self):
-		input = '0010110111'
-		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_M3", 102400))
+	def test_iq_backup_M3_424(self):
+		input = '0110101000'
+		print(execute_quantum_program([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10, input, "iq_backup_M3", 102400))
 
 if __name__ == '__main__':
 	unittest.main()
